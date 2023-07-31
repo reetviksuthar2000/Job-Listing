e9ba77199cb4b7e108b81fbdc105e4b5155186e9
reetviksuthar2000 <reetviksuthar1@gmail.com>
2023-07-31 11:54:00 +0530

    added database config and initialzation

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('node server created successfully')
-  })
-  
+app.get("/", (req, res) => {
+  res.send("node server created successfully");
+});
 
-app.listen(process.env.PORT, ()=>{
-    console.log('server listening on port http://localhost:${process.env.PORT}')
-})
+app.get("/health", (req, res) => {
+  res.status(200).send("server is up and running");
+});
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log("server running on port http://localhost:${process.env.PORT}")
+    )
+    .catch((error) => console.log(error));
+});
